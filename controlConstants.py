6bb59061d88226f0a5506985afd320ed7bfe9519
universidadpedroni <universidad.pedroni@gmail.com>
2025-02-04 17:22:36 -0300

    Se mejoró la rutina de control

--- controlConstants.py
+++ controlConstants.py
@@ -1,5 +1,7 @@
 # controlConstants.py
 
 # Ganancias de control Proporcional
-KP_POSITION = 15
-KP_STEERING = 15
+KP_POSITION = 10.0
+
+KP_STEERING = 12.0
+KI_STEERING = 0.05
--- vehicleConstants.py
+++ vehicleConstants.py
@@ -1,9 +1,12 @@
 
+
 L_car = 24.0 #[cm] Distancia ente ejes
 psi_max = 20.0 #[°] Máximo radio de giro
 v = 39.0 #[cm/seg] Velocidad lineal constante del vehículo
-r_turn_min = 70.1713    # L_car/sin(psi_max), radio mínimo de giro
+#r_turn_min = 70.1713    # L_car/sin(psi_max), radio mínimo de giro
+r_turn_min = 70.5
 
-max_power = 100
-max_angle = 100    
+max_drive_power = 100
+max_angle_power = 100
+max_angle = 127.0    
     
